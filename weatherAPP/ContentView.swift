1f290bce5f130922960af5fdfbb859937ef85da3
zishnu viknesh <shivakumar.zishnuviknesh@gmail.com>
2025-11-20 13:21:45 +0100

    Initial Commit

---
+++ weatherAPP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weatherAPP
+//
+//  Created by Zishnu Viknesh on 20.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weatherAPP/weatherAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAPPApp.swift
+//  weatherAPP
+//
+//  Created by Zishnu Viknesh on 20.11.25.
+//
+
+import SwiftUI
+
+@main
+struct weatherAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
