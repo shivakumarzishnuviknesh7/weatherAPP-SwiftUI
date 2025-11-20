baf7182944c4dd0ebb316fd63e0e55139a7e8b87
zishnu viknesh <shivakumar.zishnuviknesh@gmail.com>
2025-11-20 13:46:58 +0100

    Fist page text addition

--- weatherAPP/ContentView.swift
+++ weatherAPP/ContentView.swift
@@ -13,11 +13,22 @@ struct ContentView: View {
             //Color(.blue).edgesIgnoringSafeArea(.all)
             
             LinearGradient(
-                gradient: Gradient(colors: [ Color.red, Color.blue, Color.yellow,.white]),// to add colors 
+                gradient: Gradient(colors: [ Color.red, Color.blue, Color.yellow,.white]),// to add colors
                 startPoint: .leading,// start left
                 endPoint: .trailing// to right
             )
             .edgesIgnoringSafeArea(.all)//Ignore the safe area and draw this view all the way to the very edges of the screen.
+            //all elements are view in swift
+            
+            VStack {
+                Text("Welcome")
+                    .font(.largeTitle)
+                    .padding()
+                
+                Text("This is a weather app")
+                    .font(.title)
+                    .padding()
+            }
         }
     }
 }
